c7cb3c309493b2b09158e1dc6e22a00fb1e1eb34
baibhav-dev <baibhav@berry.hk>
2026-05-25 03:30:13 +0545

    feat: add create-task-dto and update-task-dto in tasks/dto

---
+++ api/src/tasks/dto/create-task.dto.ts
@@ -0,0 +1,39 @@
+import { TaskPriority, TaskStatus } from '@prisma/client';
+import {
+  IsDateString,
+  IsEnum,
+  IsOptional,
+  IsString,
+  IsUUID,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateTaskDto {
+  @IsString()
+  @MinLength(1, {
+    message: 'Title must be greater than 1 character.',
+  })
+  @MaxLength(100)
+  title!: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsOptional()
+  @IsEnum(TaskStatus)
+  status?: TaskStatus;
+
+  @IsOptional()
+  @IsEnum(TaskPriority)
+  priority?: TaskPriority;
+
+  @IsOptional()
+  @IsDateString()
+  dueDate?: string;
+
+  @IsOptional()
+  @IsUUID()
+  assigneeId?: string;
+}
---
+++ api/src/tasks/dto/update-task.dto.ts
@@ -0,0 +1,41 @@
+import { TaskPriority, TaskStatus } from '@prisma/client';
+import {
+  IsDateString,
+  IsEnum,
+  IsOptional,
+  IsString,
+  IsUUID,
+  MaxLength,
+  MinLength,
+  ValidateIf,
+} from 'class-validator';
+
+export class UpdateTaskDto {
+  @IsOptional()
+  @IsString()
+  @MinLength(1)
+  @MaxLength(100)
+  title?: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(1)
+  description?: string;
+
+  @IsOptional()
+  @IsEnum(TaskStatus)
+  status?: TaskStatus;
+
+  @IsOptional()
+  @IsEnum(TaskPriority)
+  priority?: TaskPriority;
+
+  @IsOptional()
+  @IsDateString()
+  dueDate?: string;
+
+  @IsOptional()
+  @ValidateIf((o) => o.assigneeId !== null)
+  @IsUUID()
+  assigneeId?: string | null;
+}
