9956f52bf8a6237334647eec72d734cac2b88ce8
BaibhavDev <baibhavkc11@gmail.com>
2026-06-16 01:17:47 +0545

    feat: update App component and integrate react-query for state management

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,7 @@
 import type { FC } from "react";
 
 const App: FC = () => {
-  return <div>App</div>;
+  return <div style={{ padding: 24 }}>DokoBoard - frontend bootstrappping...</div>;
 };
 
 export default App;
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,8 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-createRoot(document.getElementById('root')!).render(
+import App from "./App";
+import { queryClient } from "./lib/queryClient";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>,
-)
+);
